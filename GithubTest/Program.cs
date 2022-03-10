ed9605254125c7a4a46e569df8048f6bb4db69b7
DanielJhong <DanielJhong@MSI>
2022-03-10 09:05:56 +0800

    加入專案檔案。

---
+++ GithubTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+        }
+    }
+}
